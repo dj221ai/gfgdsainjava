124412d1bdaf15e0d99be37a724fcee931cb9ee2
Ved Jha <vedprakash@fanclash.in>
2023-08-17 07:58:35 +0530

    gcd naive sol

---
+++ mathprogrms/Gcd.java
@@ -0,0 +1,28 @@
+// naive approach for gcd
+
+import java.util.Scanner;
+
+public class Gcd {
+    static int gcdfinder(int a, int b){
+        while (a!=b) {
+            if (a>b) {
+                a-=b;
+            }
+            else{
+                b-=a;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int n1,n2;
+        System.out.println("Enter numbers: ");
+        try (Scanner sc = new Scanner(System.in)) {
+            n1=sc.nextInt();
+            n2=sc.nextInt();
+        }
+        int res=gcdfinder(n1, n2);
+        System.out.println("GCD is "+ res);
+    }
+}
